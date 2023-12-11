f976e0e24d4c08f6794fab73a40755753769c05d
Vishesh Bardia <developvishesh@gmail.com>
2023-12-11 20:20:41 +0530

    feat: Scrapper Class

---
+++ Scrapper.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+
+class Scrapper:
+    def __init__(self, stock: str):
+        self.stock = stock.strip().lower()
+        self.url = 'https://ticker.finology.in/company/' + stock
+        
+    def getPage(self):
+        self.page = requests.get(self.url)
+        return self.page
+     
+    def getPrice(self):
+        page = self.getPage()
+        beautifulsoup = BeautifulSoup(page._content, 'html.parser')
+
+        # print(soup)
+        result = {}
+        result['price'] = float(beautifulsoup.find("span", {"class": "currprice"}).find("span",{"class": "Number"}).text)
+        return result
+        
+
+
+if __name__ == "__main__":
+    sail = Scrapper('SAIL')
+    print(sail.getPrice())
