4e43afc0191206c9942cb0edc98ff1dc40761ad1
Vishesh Bardia <developvishesh@gmail.com>
2023-12-14 10:51:05 +0530

    feat: Combining Scrapper Class

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import requests
 from fastapi import FastAPI
-
+from Scrapper import Scrapper
 app = FastAPI()
+
+@app.get('/{stock}')
+def getprice(stock: str):
+    stock = Scrapper(stock)
+    return stock.getPrice()
